b8d372d9a7dbe8f5ed738679832a482b05b3d522
EgeniyKiselev <evgeni.kiselev@gmail.com>
2018-06-07 17:11:28 -0400

    added Waterfall Of Integration

--- src/main/java/evg/codefights/core/WaterfallOfIntegration.java
+++ src/main/java/evg/codefights/core/WaterfallOfIntegration.java
@@ -6,12 +6,44 @@ public class WaterfallOfIntegration {
 
     //for tests
     public static void main(String[] args) {
-        Arrays.stream(new WaterfallOfIntegration().boxBlur(new int[][]{
-                {7, 4, 0, 1},
-                {5, 6, 2, 2},
-                {6, 10, 7, 8},
-                {1, 4, 2, 0}
-        })).forEach(i -> System.out.println(Arrays.toString(i)));
+//        WaterfallOfIntegration w = new WaterfallOfIntegration();
+//        int row = 50; int col = 100;
+//        System.out.println("row=" + row + " col=" + col);
+//        for(int i = 0; i < 296; i++) {
+//            System.out.println(w.getIndexRow(row, col, i) + ", " + w.getIndexCol(row, col, i) + " = " + i);
+//        }
+//
+//        Arrays.stream(new WaterfallOfIntegration().contoursShifting(new int[][]{
+//                {1, 2, 3, 4},
+//                {5, 6, 7, 8},
+//                {9, 10, 11, 12},
+//                {13, 14, 15, 16},
+//                {17, 18, 19, 20}
+//        })).forEach(i -> System.out.println(Arrays.toString(i)));
+
+//        Arrays.stream(new WaterfallOfIntegration().contoursShifting(new int[][]{
+//                {238, 239, 240, 241, 242, 243, 244, 245}
+//        })).forEach(i -> System.out.println(Arrays.toString(i)));
+//        Arrays.stream(new WaterfallOfIntegration().contoursShifting(new int[][]{
+//                {238},
+//                {239},
+//                {240},
+//                {241},
+//                {242},
+//                {243},
+//                {244},
+//                {245}
+//        })).forEach(i -> System.out.println(Arrays.toString(i)));
+
+
+//        System.out.println(new WaterfallOfIntegration().getIndexCol(4, 3, 0));
+//        System.out.println(new WaterfallOfIntegration().getIndexCol(4, 3, 3));
+//        System.out.println(new WaterfallOfIntegration().getIndexCol(4, 3, 6));
+//        System.out.println(new WaterfallOfIntegration().getIndexCol(4, 3, 7));
+//        System.out.println(new WaterfallOfIntegration().getIndexCol(4, 3, 8));
+//        System.out.println(new WaterfallOfIntegration().getIndexCol(4, 3, 10));
+//        System.out.println(new WaterfallOfIntegration().getIndexCol(4, 3, 11));
+//        System.out.println(new WaterfallOfIntegration().getIndexCol(4, 3, 13));
     }
 
     boolean sudoku(int[][] grid) {
@@ -90,5 +122,60 @@ public class WaterfallOfIntegration {
             }
         }
         return res;
+    }
+
+    int[][] contoursShifting(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        System.out.println(row + " " + col);
+        for (int i = 0; i < (row + 1) / 2 && i < (col + 1) / 2; i++) {
+            int c = col - i * 2;
+            int r = row - i * 2;
+            int total;
+            if (c == 1 || r == 1) {
+                total = Math.max(c, r) - 1;
+            } else {
+                total = (r - 1) * 2 + (c - 1) * 2 - 1;
+            }
+            System.out.println(total);
+            if (i % 2 == 0) { // clockwise
+                int tmp = matrix[getIndexRow(r, c, total) + i][getIndexCol(r, c, total) + i];
+                for (int j = total; j > 0; j--) {
+                    matrix[getIndexRow(r, c, j) + i][getIndexCol(r, c, j) + i] = matrix[getIndexRow(r, c, j - 1) + i][getIndexCol(r, c, j - 1) + i];
+                }
+                matrix[getIndexRow(r, c, 0) + i][getIndexCol(r, c, 0) + i] = tmp;
+            } else {
+                int tmp = matrix[getIndexRow(r, c, 0) + i][getIndexCol(r, c, 0) + i];
+                for (int j = 0; j < total; j++) {
+                    matrix[getIndexRow(r, c, j) + i][getIndexCol(r, c, j) + i] = matrix[getIndexRow(r, c, j + 1) + i][getIndexCol(r, c, j + 1) + i];
+                }
+                matrix[getIndexRow(r, c, total) + i][getIndexCol(r, c, total) + i] = tmp;
+            }
+        }
+        return matrix;
+    }
+
+    int getIndexRow(int row, int col, int num) {
+        if (num - col < 0) { //top
+            return 0;
+        } else if (num - col - row + 1 < 0) { //right col
+            return num - col + 1;
+        } else if (num - col * 2 - row + 2 < 0) { //bottom
+            return row - 1;
+        } else {
+            return (row - 2) - (num - col - (row - 1) - (col - 1));
+        }
+    }
+
+    int getIndexCol(int row, int col, int num) {
+        if (num - col < 0) { //top
+            return num;
+        } else if (num - col - row + 1 < 0) { //right col
+            return col - 1;
+        } else if (num - col * 2 - row + 2 < 0) { //bottom
+            return col - 2 - (num - col - row + 1);
+        } else {
+            return 0;
+        }
     }
 }
