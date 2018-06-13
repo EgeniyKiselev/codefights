67db4a2d5ec1c1f58b453ce9005e0e99b6a6e709
EgeniyKiselev <evgeni.kiselev@gmail.com>
2018-06-13 15:08:56 -0400

    added Sorting Outpost task

--- src/main/java/evg/codefights/core/SortingOutpost.java
+++ src/main/java/evg/codefights/core/SortingOutpost.java
@@ -6,11 +6,9 @@ public class SortingOutpost {
 
     public static void main(String[] args) {
         SortingOutpost so = new SortingOutpost();
-        System.out.println(Arrays.toString(so.sortByLength(new String[]{"abc",
-                "",
-                "aaa",
-                "a",
-                "zz"})));
+        System.out.println(so.boxesPacking(new int[]{1, 3, 2}, new int[]{1, 3, 2}, new int[]{1, 3, 2}));
+        System.out.println(so.boxesPacking(new int[]{3, 1, 2}, new int[]{3, 1, 2}, new int[]{3, 2, 1}));
+        System.out.println(so.boxesPacking(new int[]{1, 1}, new int[]{1, 1}, new int[]{1, 1}));
     }
 
     int[] shuffledArray(int[] shuffled) {
@@ -54,5 +52,25 @@ public class SortingOutpost {
     String[] sortByLength(String[] inputArray) {
         Arrays.sort(inputArray, Comparator.comparingInt(String::length));
         return inputArray;
+    }
+
+    boolean boxesPacking(int[] length, int[] width, int[] height) {
+        int len = length.length;
+        int[][] boxes = new int[len][3];
+        for (int i = 0; i < len; i++) {
+            boxes[i][0] = length[i];
+            boxes[i][1] = width[i];
+            boxes[i][2] = height[i];
+            Arrays.sort(boxes[i]);
+        }
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (!(boxes[i][0] > boxes[j][0] && boxes[i][1] > boxes[j][1] && boxes[i][2] > boxes[j][2])
+                        && !(boxes[i][0] < boxes[j][0] && boxes[i][1] < boxes[j][1] && boxes[i][2] < boxes[j][2])) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
