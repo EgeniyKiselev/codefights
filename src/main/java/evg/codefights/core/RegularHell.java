40de57fedf9148dcd8c73682b61d112ad9d2dab2
EgeniyKiselev <evgeni.kiselev@gmail.com>
2018-06-22 16:33:31 -0400

    added RegularHell

---
+++ src/main/java/evg/codefights/core/RegularHell.java
@@ -0,0 +1,43 @@
+package evg.codefights.core;
+
+import java.util.regex.*;
+
+public class RegularHell {
+
+    public static void main(String[] args) {
+        System.out.println(new RegularHell().isSubsequence("he sd.f dsk e8.sd??l**23, 23,f.s++83+", "h  8.?*3+"));
+    }
+
+    boolean isSentenceCorrect(String sentence) {
+        String regex = "^[A-Z]+[^\\.\\?\\!]*[\\.\\?\\!]$";
+        return sentence.matches(regex);
+    }
+
+    String replaceAllDigitsRegExp(String input) {
+        return input.replaceAll("\\d", "#");
+    }
+
+
+    String swapAdjacentWords(String s) {
+        return s.replaceAll("(\\b\\w+) (\\w*\\b)", "$2 $1");
+    }
+
+    String nthNumber(String s, int n) {
+        Pattern pattern = Pattern.compile("(?:[\\D0]+)*(?:\\d+[\\D0]+){" + (n - 1) + "}(\\d+).*");
+
+        Matcher matcher = pattern.matcher(s);
+        matcher.matches();
+        return matcher.group(1);
+    }
+
+    boolean isSubsequence(String t, String s) {
+        String pattern = "";
+        for (int i = 0; i < s.length(); i++) {
+            pattern += "[\\w\\W]*" + Pattern.quote("" + s.charAt(i));
+        }
+        Pattern regex = Pattern.compile(pattern);
+        System.out.println(regex);
+        return regex.matcher(t).find();
+    }
+
+}
