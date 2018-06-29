d07f6c4599ca44f2271c5086c02c7b0f13ddf47b
EgeniyKiselev <evgeni.kiselev@gmail.com>
2018-06-29 16:01:05 -0400

    added Secret Archives

--- src/main/java/evg/codefights/core/SecretArchives.java
+++ src/main/java/evg/codefights/core/SecretArchives.java
@@ -1,13 +1,24 @@
 package evg.codefights.core;
 
+import java.util.Arrays;
 import java.util.regex.*;
 
 public class SecretArchives {
 
     public static void main(String[] args) {
 //        System.out.println(new SecretArchives().htmlTable("<table><tr><td>1</td><td>TWO</td></tr><tr><td>three</td><td>FoUr4</td></tr></table>", 1, 2));
-        System.out.println(new SecretArchives().chessNotation("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR"));
-        System.out.println(new SecretArchives().chessNotation1("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR"));
+//        System.out.println(new SecretArchives().chessNotation("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR"));
+        Arrays.stream(new SecretArchives().cellsJoining(new String[]{
+                "+----+--+-----+----+",
+                "|abcd|56|!@#$%|qwer|",
+                "|1234|78|^&=()|tyui|",
+                "+----+--+-----+----+",
+                "|zxcv|90|77777|stop|",
+                "+----+--+-----+----+",
+                "|asdf|~~|ghjkl|100$|",
+                "+----+--+-----+----+"
+        }, new int[][]{{2, 0}, {1, 1}})).forEach(t -> System.out.println(t));
+
     }
 
     String[] lrc2subRip(String[] lrcLyrics, String songLength) {
@@ -111,4 +122,65 @@ public class SecretArchives {
         }
         return sb.toString();
     }
+
+    String[] cellsJoining(String[] table, int[][] coords) {
+        int row = -1;
+        int startIndex = -1;
+        int endIndex = -1;
+        int startNum = -1;
+        int endNum = -1;
+        for (int i = 0; i < table.length; i++) {
+            if (table[i].charAt(0) == '+') {
+                row++;
+            }
+            if (row == coords[1][0] && startIndex == -1) {
+                startIndex = i;
+            }
+
+            if (row - 1 == coords[0][0] && endIndex == -1) {
+                endIndex = i;
+            }
+        }
+        int col = -1;
+        String str = table[startIndex];
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '+') {
+                col++;
+            }
+            if (col == coords[0][1] && startNum == -1) {
+                startNum = i;
+            }
+
+            if (col - 1 == coords[1][1] && endNum == -1) {
+                endNum = i;
+            }
+        }
+        for (int i = startIndex; i <= endIndex; i++) {
+            char[] chars = table[i].toCharArray();
+            for (int j = startNum; j <= endNum; j++) {
+                if (i == startIndex && j == startNum || i == startIndex && j == endNum || i == endIndex && j == startNum || i == endIndex && j == endNum) {
+                    continue;
+                }
+                if (i == startIndex || i == endIndex) {
+                    if (i == 0 || i == table.length - 1) {
+                        chars[j] = '-';
+                    }
+                    continue;
+                }
+                if (j == startNum || j == endNum) {
+                    if (j == 0 || j == chars.length - 1) {
+                        chars[j] = '|';
+                    }
+                    continue;
+                }
+                if (chars[j] == '+' || chars[j] == '|' || chars[j] == '-') {
+                    chars[j] = ' ';
+                }
+            }
+            table[i] = new String(chars);
+        }
+
+        return table;
+    }
+
 }
